6f0b24a91738242243439c04282662bd8de539d5
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-07-03 16:32:34 +0530

    new pattern added

--- 2) Advanced Pattern Programs/1_inv_tri_num.cpp
+++ 2) Advanced Pattern Programs/1_inv_tri_num.cpp
@@ -25,3 +25,31 @@ int main()
 // 123
 // 12
 // 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n:";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = n-i+1; j <= n; j++)
+        {
+            cout << j;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+// Enter n:5
+// 12345
+// 2345
+// 345
+// 45
+// 5
