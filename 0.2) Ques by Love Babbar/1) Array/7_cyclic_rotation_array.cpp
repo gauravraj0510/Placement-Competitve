0ac7f255435d5ab6bc3617588294697a9a6c26b4
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-08-09 17:00:49 +0530

    q7 done LB

---
+++ 0.2) Ques by Love Babbar/1) Array/7_cyclic_rotation_array.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void rotate(int arr[], int n);
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    int a[n], i;
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    rotate(a, n);
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+
+    return 0;
+}
+
+void rotate(int arr[], int n)
+{
+    int temp;
+    temp = arr[n - 1];
+    for (int i = n - 1; i > 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+}
+
+// 5
+// 1 2 3 4 5
+// 5 1 2 3 4
