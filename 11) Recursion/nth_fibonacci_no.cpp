a276b6f3b37b45ee642e7c41cef6d0d9edcaa8ad
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-10-03 16:22:45 +0530

    recursion bassics

--- 11) Recursion/nth_fibonacci_no.cpp
+++ 11) Recursion/nth_fibonacci_no.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int sum(int n)
+int fib(int n)
 {
     if (n == 0)
         return 0;
-    return n + sum(n - 1);
+    if (n == 1)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    cout << sum(n);
+    cout << fib(n);
     return 0;
 }
 
-// 6
-// 21
+// 5
+// 5
